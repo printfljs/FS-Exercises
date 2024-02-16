26eb1514961192f2dcd398a7700b6f008c5213a0
GanShu <console.log111@gmail.com>
2024-02-17 00:35:31 +0200

    init courseInfo app, commit 2_a_2.1

---
+++ part2/courseinfo/src/components/course.jsx
@@ -0,0 +1,31 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    <Part
+      part={parts[0]} 
+    />
+    <Part
+      part={parts[1]} 
+    />
+    <Part
+      part={parts[2]} 
+    />      
+  </>
+  
+
+const Course = ({ course }) => {
+    return (
+        <div>
+        <Header course={course.name} />
+        <Content parts={course.parts} />
+        </div>
+    )
+}
+
+export default Course
